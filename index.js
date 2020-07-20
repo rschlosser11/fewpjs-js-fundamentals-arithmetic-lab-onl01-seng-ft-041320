6b9da6507f78685d47d43abb3e7b3360cc514d9f
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-20 14:49:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 1;
+const num2 = 62;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 24 % 5;
+
+let max = Math.max(1, 2, 3, 20);
